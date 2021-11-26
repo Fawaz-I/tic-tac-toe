af3b45f58ab110c6f7124a489df3f835997fe0d8
Fawaz Ilupeju <fawazilupeju@gmail.com>
2021-11-25 20:23:22 -0500

    Im so confused rn I'm just trying to get normalize.css imported

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        options: { presets: ['env', 'react'] },
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
+    ],
+  },
+};
